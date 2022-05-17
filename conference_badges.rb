e5f1a598a0aeffa750270875b897e1ebda2927a6
Louis Y <louisy603@hotmail.com>
2022-05-17 12:57:56 -0400

    commit tree 4b32a3db0dd2d78215525fa4ecb2e3a2be5ed8d7

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+require 'pry'
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(name_array)
+    hello = name_array.map do |name|
+        "Hello, my name is #{name}."
+    end 
+end
+
+
+def assign_rooms(name_array)
+    name_array.map do |name|
+        "Hello, #{name}! You'll be assigned to room #{name_array.index(name) + 1}!"
+    end
+end
+
+def printer(name_array)
+    hello = batch_badge_creator(name_array)
+    hello.map do |name|
+        puts name
+    end 
+    rooms = assign_rooms(name_array)
+    rooms.map do |room|
+        puts room
+    end
+end
